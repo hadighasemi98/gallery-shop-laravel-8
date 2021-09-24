163b339800615ac87a62c65e1250da9d3b5877cd
hadi gh <hadi77.hg@gmail.com>
2021-09-24 13:07:48 +0330

    frontend blade created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.products.all');
 });
+
+// Route::get('home', function () {
+   
+// });
